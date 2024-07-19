a7ed2bc6f18f2a740cd140ff8b188f5c88f91a87
Ishraq Afzal <ishraq@hku.hk>
2024-07-19 15:52:20 +0800

    made changes to experience and projects section

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
-document.getElementsByClassName("buttons")
+const elements = document.getElementsByClassName("experience-mini-containers")
+
+Array.from(elements).forEach((obj) => {
+  obj.addEventListener("mouseenter", (event) => {
+    const target = event.target
+    target.querySelectorAll("*").forEach((element) => {
+      element.style.display = "none"
+    })
+  })
+
+  obj.addEventListener("mouseleave", (event) => {
+    const target = event.target
+    target.querySelectorAll("*").forEach((element) => {
+      element.style.display = "block"
+    })
+  })
+})
